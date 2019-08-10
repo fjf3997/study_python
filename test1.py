160ff3a3ebeb11c245c766f11f35b6f65050b2ce
fjf3997 <1763994902@qq.com>
2019-08-10 11:39:56 +0800

    第一次提交python文件

---
+++ test1.py
@@ -0,0 +1,10 @@
+name = input('请输入姓名:')
+age = int(input('请输入年龄:'))
+sex = input("请输入性别:")
+msg = '''
+-----------inof of %s----------
+姓名: %s
+年龄: %d
+性别: %s
+''' % (name, name, age, sex)
+print(msg)
